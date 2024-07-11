4ef4883afabf38331341af92ac5e067ef43d88be
Savan Maheshkumar Patel <sv272995@dal.ca>
2024-07-11 15:34:12 -0300

    Add : Container-1

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const axios = require('axios')
+const fs = require('fs') 
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(6000)
+
+app.post("/store-file", async (req,res) => {
+
+    const body = req.body;
+
+    const fileName = body.file;
+    var data = body.data;
+
+    if(fileName == null || fileName == ""){
+        const response = {
+            file:null,
+            error:"Invalid JSON input."
+        }
+
+        res.status(400).json(response)
+    }
+
+    data = data.split('\n').map(line => 
+        line.split(',').map(field => field.trim()).join(',')
+      ).join('\n');
+
+    const filePath = "/data/" + fileName;
+
+    fs.writeFile(filePath, data, "utf-8", (err) => {
+
+        if(err){
+            const response = {
+                file: fileName,
+                error: "Error while storing the file to the storage."
+            }
+
+            res.status(500).json(response)
+        }
+
+        const response = {
+            file:fileName,
+            message:"Success."
+        }
+
+        res.status(200).json(response)
+    })
+})
+
+app.post("/calculate",async (req,res)=>{
+
+    const body = req.body;
+
+    const fileName = body.file;
+
+    if(fileName == null || fileName == ""){
+        const response = {
+            file: null,
+            error: "Invalid JSON input."
+        }
+
+        res.status(400).json(response)
+    }else{
+        
+        const filePath = "/data/" + fileName;
+
+        if(fs.existsSync(filePath)){
+            
+            try{
+                const result = await axios.post("http://container2-service:80/result",body)
+
+                res.json(result.data)
+                
+            }catch(error){
+                if(error.response){
+                    res.status(error.response.status).json(error.response.data);
+                }else{
+                    res.status({error:"Internal Server error"})
+                }
+                  
+            }
+          
+        }else{
+            const response = {
+                file:fileName,
+                error:"File not found."
+            }
+            res.status(404).json(response)
+        }
+    }
+})
